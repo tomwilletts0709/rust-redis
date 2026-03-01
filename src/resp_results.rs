a4c22552fc4ef2eab7c4214792ffe760a0a35040
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-01 21:02:58 +0000

    adding ParseInt

--- src/resp_results.rs
+++ src/resp_results.rs
@@ -1,21 +1,33 @@
 use std::fmt;
+use std::num;
+use std::string::FromUtf8Error;
 
 #[derive(Debug)]
 pub enum RESPError {
     FromUtf8,
+    IncorretLength(RESPLength),
     OutOfBounds(usize),
     Unknown,
-    WrongType
+    WrongType,
+    ParseInt,
 }
 
 impl fmt::Display for RESPError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             RESPError::OutOfBounds(index) => write!(f, "Out of bounds: {}", index),
+            RESPError::IncorretLength(length) => write!(f, "Incorrect length: {}", length),
             RESPError::FromUtf8 => write!(f, "Error converting bytes to string"),
             RESPError::Unknown => write!(f, "Unknown error"),
             RESPError::WrongType => write!(f, "Wrong type"),
+            RESPError::ParseInt => write!(f, "Error parsing integer"),
         }
+    }
+}
+
+impl From<FromUtf8Error> for RESPError {
+    fn from(_err: num::ParseIntError) -> Self {
+        Self::ParseInt
     }
 }
 
