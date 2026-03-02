20e77a2845d15ce791db165a5c6b63cd3be28430
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-02 14:40:53 +0000

    strings.rs

---
+++ src/commands/strings.rs
@@ -0,0 +1,39 @@
+use crate::(
+    resp::RESP,
+    storage_result::StorageResult,
+    storage::Storage,
+    error::Error,
+);
+
+pub struct SetCommand {
+    pub key: String,
+    pub value: String,
+}
+
+impl Command for SetCommand {
+    fn execute(&self, args: &[String]) -> StorageResult<RESP> {
+        match args {
+            [key, value] => {
+                store.set(key.to_string(), value.as_bytes().to_vec());
+                Ok(RESP::SimpleString("OK".to_string()))
+            }
+            _ => Err(StorageError::CommandSyntaxError("SET requires 2 arguments".into())),
+        }
+    }
+}
+
+pub struct GetCommand {
+    pub key: String,
+}
+
+impl Command for GetCommand {
+    fn execute(&self, args: &[String]) -> StorageResult<RESP> {
+        match args {
+            [key] => match store.get(key) {
+                Some(bytes) => Ok(RESP::BulkString(String::from_utf8(bytes).unwrap())),
+                None => Ok(RESP::Null),
+            }
+            _ => Err(StorageError::CommandSyntaxError("GET requires 1 argument".into())),
+        }
+    }
+}
